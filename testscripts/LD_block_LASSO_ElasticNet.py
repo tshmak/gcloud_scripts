2933b373079924fe5c0d39bc80a74ffaa06e6a51
leezx <zxlee49@613.com>
2018-06-04 19:59:58 +0800

    additional_init

--- testscripts/LD_block_LASSO_ElasticNet.py
+++ testscripts/LD_block_LASSO_ElasticNet.py
@@ -1,19 +1,4 @@
-# coding: utf-8
 import hail as hl
-import hail.expr.aggregators as agg
-import numpy as np
-import matplotlib.pyplot as plt
-from math import log, isnan
-from pprint import pprint
-import time
-hl.init() # Initialize Hail and Spark.
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
-
 mt = hl.balding_nichols_model(3, 100, 100)
 gts_as_rows = mt.annotate_rows(
   mean = hl.agg.mean(hl.float(mt.GT.n_alt_alleles()))
@@ -28,20 +13,13 @@ groups = gts_as_rows.group_by(
 
 df = groups.to_spark()
 
-import findspark
-findspark.init()
-
-import pyspark # only run after findspark.init()
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.getOrCreate()
-
 from pyspark.sql.functions import udf
 
 def get_intercept(X, y):
+    from sklearn import linear_model
     clf = linear_model.Lasso(alpha=0.1)
     clf.fit(X, y)
     return float(clf.intercept_)
 
 get_intercept_udf = udf(get_intercept)
 df.select(get_intercept_udf("genotypes", "ys").alias("intercept")).show()
-
